9b1130cc8f44dfbca94a6a32aa0e659af2985242
Stanislav Bulgakov <sbulgakov@hse.ru>
2023-05-14 16:48:08 +0300

    only report needed

--- Matrix.hpp
+++ Matrix.hpp
@@ -1,268 +1,82 @@
 #ifndef MATRIX_TEMPLATE
 #define MATRIX_TEMPLATE 09022023L
 
+#include <iostream>
 
-template<typename T>
+template <typename T>
 class Matrix {
-    private: 
-        T *data;
-        size_t rows;
-        size_t columns;
+    private:
+        T* data_;
+        size_t side_size_;
+        size_t squared_size_;
     
     public:
-        Matrix(): data(NULL), rows(0), columns(0) {};
-        Matrix(const Matrix<T>& other): rows(other.rows), columns(other.columns) {
-            data = new T[rows * columns];
-            size_t length = rows * columns;
-            for (size_t i = 0; i < length; ++i) {
-                data[i] = other.data[i];
+        long coefficient = 1;
+        Matrix(): side_size_(0), data_(NULL), squared_size_(0) {}
+        Matrix(size_t size): side_size_(size), data_(new T[size * size]), squared_size_(size * size) {}
+        Matrix(T* data, size_t size): side_size_(size), squared_size_(size * size) {
+            data_ = new T[squared_size_];
+            for (size_t i = 0; i < squared_size_; ++i) {
+                data_[i] = data[i];
             }
-        };
-        Matrix(size_t rows, size_t cols): rows(rows), columns(cols) {
-            data = new T[rows * columns]();
-        };
-        ~Matrix() {
-            delete[] data;
-            this->data = NULL;
         }
-        
-        Matrix<T>& operator=(const Matrix<T>& other) {
-            if (&other == this) {
-                return *this;
-            }
+        ~Matrix() {
+            delete[] data_;
+        }
 
-            if (data) delete[] data;
+        Matrix& operator=(const Matrix& other) {
+            if (this == &other) return *this;
 
-            rows = other.rows;
-            columns = other.columns;
-            
-            size_t length = rows * columns;
-            data = new T[length];
+            if (data_) delete[] data_;
 
-            for (size_t i = 0; i < length; ++i) {
-                data[i] = other.data[i];
+            this->side_size_ = other.side_size_;
+            this->squared_size_ = other.squared_size_;
+            this->data_ = new T[this->squared_size_];
+            this->coefficient = other.coefficient;
+
+            for (size_t i = 0; i < this->squared_size_; ++i) {
+                data_[i] = other.data_[i];
             }
 
             return *this;
-        };
+        } 
 
-        friend std::ostream& operator<<(std::ostream& out, const Matrix& matr) {
-            for (size_t i = 0; i < matr.columns; ++i) {
-                for (size_t y = 0; y < matr.rows; ++y) {
-                    out << matr.data[i * matr.rows + y] << '\t';
-                }
-                out << std::endl;
-            }
-
-            return out;
+        size_t side() const {
+            return side_size_;
         }
 
-        friend std::istream& operator>> (std::istream& in, Matrix<T>& matrix) {
-            size_t i, columns, rows;
-            T current;
-
-            std::cin >> columns;
-            std::cin >> rows;
-
-            T* new_data = new T[columns * rows];
-
-            for (i = 0; i < columns * rows; ++i) {
-                std::cin >> current;
-                new_data[i] = current;
-            }
-
-            matrix.columns = columns;
-            matrix.rows = rows;
-            matrix.data = new_data;
-
-            return in;
+        size_t size() const {
+            return squared_size_;
         }
 
-        T&      at(size_t r, size_t c) {
-            return data[c * columns + r];
-        };
-        const T& at(size_t r, size_t c) const {
-            return data[c * columns + r];
-        };
-
-        friend Matrix<T> operator+(const Matrix<T>& left, const Matrix<T>& right) {
-            Matrix<T> to_return (left.rows, left.columns);
-            size_t length = left.rows * left.columns;
-            for (size_t i = 0; i < length; ++i) {
-                to_return.data[i] = left.data[i] + right.data[i];
-            }
-
-            return to_return;
+        T& at(size_t index) {
+            return data_[index];
+        }
+        const T& at(size_t index) const {
+            return data_[index];
+        }
+        T& at(size_t columns, size_t rows) {
+            return data_[side_size_ * columns + rows];
+        }
+        const T& at(size_t columns, size_t rows) const {
+            return data_[side_size_ * columns + rows];
         }
 
-        friend Matrix<T> operator*(const Matrix<T>& left, const Matrix<T>& right) {
-            Matrix<T> to_return (left.rows, left.columns); 
-            /*
-                i <=> current_row_second_matrix
-                u <=> current_column_first_matrix
-                j <=> pointer
-            */
-            for (size_t i = 0; i < right.rows; ++i) {
-                for (size_t u = 0; u < left.columns; ++u) {
-                    for (size_t j = 0; j < left.rows; ++j) {
-                        to_return.at(i, u) += left.at(j, u) * right.at(i, j);
-                    }
-                }
-            }
-
-            return to_return;
-        }
-
-        template <typename Integer>
-        friend Matrix<T> operator*(const Matrix<T>& left, const Integer& number) {
-            Matrix to_return (left);
-            size_t length = left.rows * left.columns;
-
-            for (size_t i = 0; i < length; ++i) {
-                to_return.data[i] *= number;
-            }
-
-            return to_return;
-        }
-
-        template <typename Integer>
-        friend Matrix<T> operator*(const Integer& number, const Matrix<T>& left) {
-            return left * number;
-        }
-
-        template <typename Integer>
-        friend Matrix<T> operator/(const Matrix<T>& left, const Integer& number) {
-            return left * (1.0 / number);
-        }
-
-        friend Matrix<T> operator-(const Matrix<T>& left, const Matrix<T>& right) {
-            Matrix negative_copy (right);
-            return left + (-1) * negative_copy;
-        }
-
-        T&       operator()(size_t r, size_t c)       {
-            return this->at(r, c);
-        }
-        const T& operator()(size_t r, size_t c) const {
-            return this->at(r, c);
-        }
-        
-        void Transpose() {
-            Matrix<T> result (this->columns, this->rows);
-
-            for (size_t i = 0; i < this->columns; ++i) {
-                for (size_t y = 0; y < this->rows; ++y) {
-                    result.at(i, y) = this->at(y, i);
-                }
-            }
-
-            delete[] this->data;
-            this->data = result.data;
-            result.data = NULL;
-        };
-        
-        Matrix<T>& M() const {
-            Matrix<T> *M_MATRIX = new Matrix<T>();
-            this->LU(NULL, NULL, M_MATRIX);
-            return *M_MATRIX;
-        };
-
-        Matrix<T>& L() const {
-            Matrix<T> *L_MATRIX = new Matrix<T>();
-            this->LU(NULL, L_MATRIX, NULL);
-            return *L_MATRIX;
-        }
-
-        Matrix<T>& U() const {
-            Matrix<T> *U_MATRIX = new Matrix<T>();
-            this->LU(U_MATRIX, NULL, NULL);
-            return *U_MATRIX;
-        }
-        
-        // если u != 0, записывает в u верхнетреугольную матрицу
-        // если l != 0, записывает в l нижнетреугольную матрицу
-        // если m != 0, записывает в m матрицу перестановок
-        void LU(Matrix<T> *u, Matrix<T> *l, Matrix<T> *m) const {
-            size_t i, y, counter;
-            double coeff;
-            Matrix<T> M_MATRIX (this->rows, this->columns);
-            Matrix<T> U_MATRIX (*this);
-            Matrix<T> L_MATRIX (this->rows, this->columns);
-
-            for (i = 0; i < this->columns; ++i) {
-                for (y = 0; y < this->rows; ++y) {
-                    if (i == y) {
-                        M_MATRIX.at(y, i) = 1;
-                        L_MATRIX.at(y, i) = 1;
-                    }
-                }
-            }
-
-            /*
-                i <=> current leading column and row
-                y <=> current leading row
-                counter <=> pointer
-            */
-
-            for (i = 0; i < this->columns; ++i) {
-                for (counter = i + 1; counter < this->columns; ++counter) {
-                    coeff = U_MATRIX.at(i, counter) * 1.0 / U_MATRIX.at(i, i) * 1.0;
-                    L_MATRIX.at(i, counter) = coeff;
-                    for (y = i; y < this->rows; ++y) {
-                        U_MATRIX.at(y, counter) -= U_MATRIX.at(y, i) * coeff;
-                    }
-                }
-            }
-
-            if (u != NULL) {
-                *u = U_MATRIX;
-            }
-            if (l != NULL) {
-                *l = L_MATRIX;
-            }
-            if (m != NULL) {
-                *m = M_MATRIX;
-            }
-        };
-        
-        // решение СЛАУ A*x=b
-        // вход:  b - матрица размера rows*1
-        // воход: x - матрица размера rows*1
-        Matrix<T>& Solve(const Matrix<T>& b) const {
-            /*
-            L * X = B
-            https://courses.physics.illinois.edu/cs357/sp2020/notes/ref-9-linsys.html
-            l11 * x1 = b1
-            l12 * x1 + l22 * x2 = b2
-            ...
-            */ 
-            T current_x;
-            size_t i, y;
-            Matrix<T> *X = new Matrix(1, this->rows);
-            Matrix<T> X_MATRIX (1, this->rows);
-            Matrix<T> RES_MATRIX (1, this->rows);
-            Matrix<T> L_MATRIX, U_MATRIX;
-            this->LU(&U_MATRIX, &L_MATRIX, NULL); 
-            for (i = 0; i < this->rows; ++i) {
-                current_x = b.at(i, 0);
-                for (y = 0; y < i; ++y) {
-                    current_x = current_x - X_MATRIX.at(y, 0) * L_MATRIX.at(y, i);
-                }
-                X_MATRIX.at(i, 0) = current_x / L_MATRIX.at(i, i);
-            }
-
-            for (i = this->rows - 1; i > -1; --i) {
-                current_x = X_MATRIX.at(i, 0);
-                for (y = i + 1; y < this->rows; ++y) {
-                    current_x -= U_MATRIX.at(y, i) * RES_MATRIX.at(y, 0);
-                }
-                RES_MATRIX.at(i, 0) = current_x;
-            }
-
-            *X = RES_MATRIX;
-            return *X;
-        };
+        template <class U>
+        friend std::ostream& operator<< (std::ostream& out, const Matrix<U>& matrix);
         
 };
+
+template <class U>
+std::ostream& operator<<(std::ostream& out, const Matrix<U>& matrix) {   
+    size_t i, u;
+    for (i = 0; i < matrix.side_size_; ++i) {
+        for (u = 0; u < matrix.side_size_; ++u) {
+            out << matrix.at(i, u) << '\t';
+        }
+        out << std::endl;
+    }
+    return out; 
+}
 
 #endif /* MATRIX_TEMPLATE */
--- main.cpp
+++ main.cpp
@@ -1,53 +1,133 @@
 #include <iostream>
+#include <thread>
+#include <chrono>
+#include "Matrix.hpp"
 
-long determinant_of_2d(long* numbers) {
-    return numbers[0] * numbers[2] - numbers[1] * numbers[3];
+template <typename T>
+long determinant(const Matrix<T>& matrix) {
+    if (matrix.side() == 1) return matrix.coefficient * matrix.at(0);
+    return matrix.coefficient * (matrix.at(0) * matrix.at(2) - matrix.at(1) * matrix.at(3));
 }
 
-long** matrix_below(long* numbers, size_t size) {
-    long** to_return = new long*[size];
-    long current_coef;
-    long* current_array;
-    size_t i, u, counter, squared_size = size * size;
-    for (i = 0; i < size; ++i) {
-        current_coef = numbers[i];
-        current_array = new long[(size - 1) * (size - 1)];
+template <typename T>
+Matrix<T>* matrix_below(const Matrix<T>& base) {
+    size_t i, u, counter;
+    Matrix<T>* to_return = new Matrix<T>[base.side()]();
+    Matrix<T> current (base.side() - 1);
+    for (i = 0; i < base.side(); ++i) {
+        current.coefficient = base.coefficient * base.at(i);
+        if (i % 2 != 0) current.coefficient *= (-1);
         counter = 0;
-        for (u = 0; u < squared_size; ++u) {
-            if ((u % size != i) && (u / size != 0)) {
-                current_array[counter++] = current_coef * numbers[u]; // Передача массива по ссылке с последующей записью коэффициента
+        for (u = 0; u < base.size(); ++u) {
+            if ((u + 1 > base.side()) && (u % base.side() != i)) {
+                current.at(counter) = base.at(u);
+                ++counter;
             }
         }
-        to_return[i] = current_array;
+        to_return[i] = current;
     }
     return to_return;
 }
 
-void print_matrix(long* matrix, size_t size) {
-    for (size_t i = 0; i < size * size; ++i) {
-        if (i % size == 0) std::cout << '\n';
-        std::cout << matrix[i] << '\t';
+template <typename T>
+long calculateDeterminant(const Matrix<T>& base, long& to_write) {
+    size_t i, u, current_side_size, current_size = 1;
+    long to_return = 0;
+    Matrix<T>* returned_matrixes = NULL, *current_matrixes = NULL;
+    Matrix<T>* matrixes = new Matrix<T>[current_size]();
+    matrixes[0] = base;
+    while (matrixes[0].size() > 0) {
+        if (matrixes[0].size() <= 2) {
+            for (i = 0; i < current_size; ++i) {
+                to_return += determinant(matrixes[i]);
+            }
+            break;
+        }
+
+        if (current_matrixes) delete[] current_matrixes;
+        current_side_size = matrixes[0].side();
+        current_matrixes = new Matrix<T>[current_size * current_side_size]();
+        for (i = 0; i < current_size; ++i) {
+            returned_matrixes = matrix_below(matrixes[i]);
+            for (u = 0; u < current_side_size; ++u) {
+                current_matrixes[i * current_side_size + u] = returned_matrixes[u];
+            }
+        }
+        current_size *= current_side_size;
+        delete[] matrixes;
+        matrixes = new Matrix<T>[current_size]();
+        for (i = 0; i < current_size; ++i) {
+            matrixes[i] = current_matrixes[i];
+        }
     }
+
+    to_write += to_return;
+
+    return to_return;
 }
 
+template <typename T>
+long linear_algorithm(const Matrix<T>& matrix) {
+    long determinant = 0;
+    calculateDeterminant<long>(matrix, std::ref(determinant));
+    return determinant;
+}
 
-// size - длина строки / столбца
-// long** determinants(long* numbers, size_t size) {
-//     size_t i, u, current_size;
-//     long** current[size];
-//     for (i = size; i > 2; --i) {
-//         for (u = 0; u < )
-//     }
-// }
+template <typename T>
+long parallel_algorithm(const Matrix<T>& matrix) {
+    long determinant = 0;
+    Matrix<T>* matrixes = matrix_below(matrix);
+    for (size_t i = 0; i < matrix.side(); ++i) {
+        std::thread current (calculateDeterminant<T>, matrixes[i], std::ref(determinant));
+        current.join();
+    }
+    return determinant;
+}
+
+Matrix<long> defaultMatrix() {
+    int size_of_matrix = 4;
+    long* numbers = new long[16]{11, 19, 3, 12, 2, 86, 43, 16, 64, 3, 7, 4, 6, 4, 13, 6};
+    Matrix<long> matrix (numbers, size_of_matrix);
+    return matrix;
+}
+
+Matrix<long> randomMatrix() {
+    srand(time(0));
+    size_t size_of_matrix = 4 + rand() % 6;
+    long* numbers = new long[size_of_matrix * size_of_matrix];
+    for (size_t i = 0; i < size_of_matrix * size_of_matrix; ++i) {
+        numbers[i] = rand() % 40;
+    } 
+    Matrix<long> matrix (numbers, size_of_matrix);
+    return matrix;
+}
+
+template <typename T>
+void checkAlgorithm(const Matrix<T>& matrix) {
+    long determinant1 = 0;
+    long determinant2 = 0;
+
+    std::cout << matrix << std::endl << std::endl;
+
+    auto start1 = std::chrono::steady_clock::now();
+    determinant1 = linear_algorithm<long>(matrix);
+    auto end1 = std::chrono::steady_clock::now();
+    std::chrono::duration<double> elapsed_linear = end1 - start1;
+
+    auto start2 = std::chrono::steady_clock::now();
+    determinant2 = parallel_algorithm<long>(matrix);
+    auto end2 = std::chrono::steady_clock::now();
+    std::chrono::duration<double> elapsed_parallel = end2 - start2;
+
+    std::cout << "Linear algorithm's determinant: " << determinant1 << std::endl;
+    std::cout << "Linear algorithm's time (s): " << elapsed_linear.count() << std::endl;
+    std::cout << "Parallel algorithm's determinant: " << determinant2 << std::endl;
+    std::cout << "Parallel algorithm's time (s): " << elapsed_parallel.count() << std::endl << std::endl;
+}
 
 int main() {
-    long* smth = new long[9]{1, 2, 3, 4, 5, 6, 7, 8, 9};
-
-    long** wtf = matrix_below(smth, 3);
-    for (size_t i = 0; i < 3; ++i) {
-        std::cout << std::endl;
-        print_matrix(wtf[i], 2);
-    }
-
+    Matrix<long> matrix = defaultMatrix();    
+    checkAlgorithm(matrix);
+    checkAlgorithm(randomMatrix());
     return 0;
 }
